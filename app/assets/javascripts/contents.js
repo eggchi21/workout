181c897eb626d28c42c4b3665c8756b0059b5fc8
eggchi21 <eggchi21@yahoo.co.jp>
2019-10-26 16:33:06 +0900

    add header and about page detail

--- app/assets/javascripts/contents.js
+++ app/assets/javascripts/contents.js
@@ -1,31 +1,31 @@
-$(function(){
-  var exit_count = 0;
-  var elems_count = 0;
-  function repeat_show(elems$) {
-    elems_count = elems$.length;
-    elems$.eq(exit_count).show('fast', function() {
-      exit_count ++;
-      if(exit_count < elems_count) {
-        repeat_show(elems$);
-      }
-    });
-  }
+// $(function(){
+//   var exit_count = 0;
+//   var elems_count = 0;
+//   function repeat_show(elems$) {
+//     elems_count = elems$.length;
+//     elems$.eq(exit_count).show('fast', function() {
+//       exit_count ++;
+//       if(exit_count < elems_count) {
+//         repeat_show(elems$);
+//       }
+//     });
+//   }
 
-  $(document).ready(function(){
-     repeat_show($(".contents__list-box"));
-  });
-  $("li").hover(
-    function () {
-      $(this).find("*").css({
-        color: "#4dc0b2",
-        backgroundColor: "#ffc042"
-      });
-    },
-    function () {
-      $(this).find("*").css({
-        color: "",
-        backgroundColor: ""
-      });
-    }
-  );
-})
+//   $(document).ready(function(){
+//      repeat_show($(".contents__list-box"));
+//   });
+//   $("li").hover(
+//     function () {
+//       $(this).find("*").css({
+//         color: "#4dc0b2",
+//         backgroundColor: "#ffc042"
+//       });
+//     },
+//     function () {
+//       $(this).find("*").css({
+//         color: "",
+//         backgroundColor: ""
+//       });
+//     }
+//   );
+// })
--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,5 +1,6 @@
 class HomesController < ApplicationController
   def index
+    redirect_to about_homes_path unless user_signed_in?
   end
   def about
   end
