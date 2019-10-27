7947902dd85bd82c13ffec4951eb86906c7c2345
eggchi21 <eggchi21@yahoo.co.jp>
2019-10-27 16:37:32 +0900

    add 1week after weight

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,43 +1,57 @@
 class Report < ApplicationRecord
   belongs_to :user
   has_many_attached :images
-  def ols
-    # xs = [50,60,70,80,90]
-    # ys = [40,70,90,60,100]
+  def self.ols(reports)
+    ys = reports.map(&:weight)
+    xs = date_to_axis(reports.map(&:entry_on))
 
+    array = xs.zip(ys)
+    a = array.convariance / xs.variance # 回帰直線の傾き(a = XとYの共分散 / Xの分散)
+    b = ys.average - a * xs.average # 回帰直線の切片(b = Yの平均 - 回帰直線の傾き * Xの平均)
+    # {a:a,b:b}
 
-    xs = [1,2,3,4,5]
-    ys = [2,4,6,8,10]
-    arry = xs.zip(ys)
+    week_after = (a * (xs.last + 7) + b).round(1)
 
-    a = arry.convariance/xs.variance #回帰直線の傾き(a = XとYの共分散 / Xの分散)
-    b = ys.average - a * xs.average #回帰直線の切片(b = Yの平均 - 回帰直線の傾き * Xの平均)
-    {a:a,b:b}
+  end
+
+  def self.date_to_axis(array)
+    axis =[]
+    before_elem = nil
+    array.each.with_index(1) do | elem , i |
+      if i == 1
+        @axis_elem = 1
+      else
+        @axis_elem = @axis_elem + (elem - before_elem).numerator
+      end
+      before_elem = elem
+      axis << @axis_elem
+    end
+    return axis
   end
 
 end
 
 
 class Array
-  def average #平均
+  def average # 平均
     sum.to_f / size
   end
 
-  def variance #分散
+  def variance # 分散
     inject(0) { |result,n| result + (n - average) ** 2 } / size
   end
 
-  def unbiasedvariance #不偏分散
+  def unbiasedvariance # 不偏分散
     inject(0) { |result,n| result + (n - average) ** 2 } / (size - 1)
   end
 
-  def stadiv #標準偏差
+  def stadiv # 標準偏差
     Math.sqrt(variance)
   end
 
-  def convariance #共分散
-    ary1 = map{|xs| xs[0]}
-    ary2 = map{|ys| ys[1]}
-    inject(0) { |result,n| result + (n[0] - ary1.average) * (n[1] - ary2.average)}/ size
+  def convariance # 共分散
+    array1 = map{|xs| xs[0]}
+    array2 = map{|ys| ys[1]}
+    inject(0) { |result,n| result + (n[0] - array1.average) * (n[1] - array2.average)}/ size
   end
 end
