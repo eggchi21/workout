bae1ed8554630f80e6f71d423201a1ca2e3befb9
eggchi21 <eggchi21@yahoo.co.jp>
2019-10-27 23:50:54 +0900

    add refinement and fdiv

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,14 +1,14 @@
 class Report < ApplicationRecord
   belongs_to :user
   has_many_attached :images
+
   def self.ols(reports)
     ys = reports.map(&:weight)
     xs = date_to_axis(reports.map(&:entry_on))
 
     array = xs.zip(ys)
-    a = array.convariance / xs.variance # 回帰直線の傾き(a = XとYの共分散 / Xの分散)
+    a = array.convariance.fdiv(xs.variance) # 回帰直線の傾き(a = XとYの共分散 / Xの分散)
     b = ys.average - a * xs.average # 回帰直線の切片(b = Yの平均 - 回帰直線の傾き * Xの平均)
-    # {a:a,b:b}
 
     week_after = (a * (xs.last + 7) + b).round(1)
 
@@ -31,27 +31,30 @@ class Report < ApplicationRecord
 
 end
 
+module ArrayStatistics
+  refine Array do
+    def average # 平均
+      sum.fdiv(size)
+    end
 
-class Array
-  def average # 平均
-    sum.to_f / size
-  end
+    def variance # 分散
+      @average = average
+      inject(0) { |result,n| result + (n - @average) ** 2 }.fdiv(size)
+    end
 
-  def variance # 分散
-    inject(0) { |result,n| result + (n - average) ** 2 } / size
-  end
+    def unbiasedvariance # 不偏分散
+      @average = average
+      inject(0) { |result,n| result + (n - @average) ** 2 }.fdiv(size - 1)
+    end
 
-  def unbiasedvariance # 不偏分散
-    inject(0) { |result,n| result + (n - average) ** 2 } / (size - 1)
-  end
+    def stadiv # 標準偏差
+      Math.sqrt(variance)
+    end
 
-  def stadiv # 標準偏差
-    Math.sqrt(variance)
-  end
-
-  def convariance # 共分散
-    array1 = map{|xs| xs[0]}
-    array2 = map{|ys| ys[1]}
-    inject(0) { |result,n| result + (n[0] - array1.average) * (n[1] - array2.average)}/ size
+    def convariance # 共分散
+      array1 = map{|xs| xs[0]}
+      array2 = map{|ys| ys[1]}
+      inject(0) { |result,n| result + (n[0] - array1.average) * (n[1] - array2.average)}.fdiv(size)
+    end
   end
 end
