f3abc7dd4b53671ecb15f0fb0e1c67f7f0e651bb
eggchi21 <eggchi21@yahoo.co.jp>
2019-11-12 20:37:44 +0900

    change seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 Dir.glob(File.join(Rails.root, 'db', 'seeds', 'user.rb')) do |file|
   load(file)
 end
-Dir.glob(File.join(Rails.root, 'db', 'seeds', 'report.rb')) do |file|
-  load(file)
-end
+# Dir.glob(File.join(Rails.root, 'db', 'seeds', 'report.rb')) do |file|
+#   load(file)
+# end
