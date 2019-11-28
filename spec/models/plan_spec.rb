3b89633433543a3b6a38ffd18a34daac6a93bd0f
eggchi21 <eggchi21@yahoo.co.jp>
2019-11-28 22:51:56 +0900

    Update plan_spec.rb

--- spec/models/plan_spec.rb
+++ spec/models/plan_spec.rb
@@ -9,6 +9,12 @@ describe Plan do
       expect(plan).to be_valid
     end
 
+    # --- 全部
+    it "is valid with a start_weight,target_weight,start_on, target_on, method, protein, fat,carbo, user_id" do
+      user = create(:user)
+      plan = build(:plan,user_id: user.id)
+      expect(plan).to be_valid
+    end
   end
 
 end
