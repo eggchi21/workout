cf72f2b80438ff6bb89c4f7038dd9fb60f50a1df
eggchi21 <eggchi21@yahoo.co.jp>
2019-12-02 20:53:42 +0900

    edit plan create test

--- spec/features/plan_spec.rb
+++ spec/features/plan_spec.rb
@@ -18,14 +18,26 @@ feature 'plan', type: :feature do
     expect(page).to have_content('目標を作成する')
 
     # planの投稿
-    # expect {
-    #   click_link('目標を作成する')
-    #   expect(current_path).to eq new_plan_path
-    #   fill_in 'plan[start_weight]', with: '80'
-    #   fill_in 'plan[target_weight]', with: '77'
-    #   fill_in 'plan[start_on]', with: '2019/10/28'
-    #   fill_in 'plan[target_on]', with: '2019/12/28'
-    #   find('input[type="submit"]').click
-    # }.to change(Plan, :count).by(1)
+    expect {
+      click_link('目標を作成する')
+      expect(current_path).to eq "/users/#{user.id}/edit"
+      fill_in 'user[nickname]', with: 'test'
+      fill_in 'user[introduction]', with: 'testdayo'
+      choose '男性'
+      select '3回～4回/週 程度の運動', from: '活動レベル'
+      fill_in 'user[age]', with: '27'
+      fill_in 'user[height]', with: '170'
+      find('input[type="submit"]').click
+      expect(current_path).to eq new_plan_path
+      fill_in 'plan[start_weight]', with: '80'
+      fill_in 'plan[target_weight]', with: '77'
+      fill_in 'plan[start_on]', with: '2019/12/02'
+      fill_in 'plan[target_on]', with: '2020/01/25'
+      choose 'ローファットダイエット'
+      fill_in 'plan[protein]', with: '276'
+      fill_in 'plan[fat]', with: '31'
+      fill_in 'plan[carbo]', with: '345'
+      find('input[type="submit"]').click
+    }.to change(Plan, :count).by(1)
   end
 end
