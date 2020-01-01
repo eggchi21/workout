1d8c51f05c17cf773bcbde180605a5e1932cb65e
eggchi21 <eggchi21@yahoo.co.jp>
2020-01-01 19:08:38 +0900

    edit credentialyml and auth path

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -260,13 +260,10 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  # if Rails.env.production?
-  #   config.omniauth :facebook, ENV['FACEBOOK_APP_ID_PRO'], ENV['FACEBOOK_APP_SECRET_PRO']
-  #   config.omniauth :google_oauth2, ENV['GOOGLE_APP_ID_PRO'], ENV['GOOGLE_APP_SECRET_PRO']
-  # else
-    config.omniauth :facebook, Rails.application.credentials.facebook[:FACEBOOK_APP_ID_DEV], Rails.application.credentials.facebook[:FACEBOOK_APP_SECRET_DEV]
-    config.omniauth :google_oauth2, Rails.application.credentials.google_oauth2[:GOOGLE_APP_ID_DEV], Rails.application.credentials.google_oauth2[:GOOGLE_APP_SECRET_DEV]
-  # end
+
+  config.omniauth :facebook, Rails.application.credentials.facebook[:FACEBOOK_APP_ID], Rails.application.credentials.facebook[:FACEBOOK_APP_SECRET]
+  config.omniauth :google_oauth2, Rails.application.credentials.google_oauth2[:GOOGLE_APP_ID], Rails.application.credentials.google_oauth2[:GOOGLE_APP_SECRET]
+
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
